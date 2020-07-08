b23dc3846d6565cedebf325870ab58ad6a72a61f
Jan Starczewski <jan.a.starczewski@gmail.com>
2020-07-08 17:33:53 +0200

    Create sample project

---
+++ src/Application.kt
@@ -0,0 +1,20 @@
+package com.softwaret
+
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused")
+fun Application.module() {
+
+    routing {
+        get("/check") {
+            call.respondText("KPDF")
+        }
+    }
+}
+
