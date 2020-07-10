5c80db710ad365138480b2caa014e440892ee9e9
Jan Starczewski <jan.a.starczewski@gmail.com>
2020-07-10 11:35:07 +0200

    Fix kodein dependencies

--- src/Application.kt
+++ src/Application.kt
@@ -5,6 +5,7 @@ import io.ktor.application.call
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
+import org.kodein.di.Kodein
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -17,4 +18,3 @@ fun Application.module() {
         }
     }
 }
-
