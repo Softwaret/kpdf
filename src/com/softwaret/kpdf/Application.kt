7aaffd9542da82469dbf68f93698d03ff3f19ee9
Jan Starczewski <jan.a.starczewski@gmail.com>
2020-07-10 20:30:19 +0200

    Update Application

--- src/com/softwaret/kpdf/Application.kt
+++ src/com/softwaret/kpdf/Application.kt
@@ -1,10 +1,16 @@
 package com.softwaret.kpdf
 
 import com.softwaret.kpdf.controller.bindControllers
+import com.softwaret.kpdf.interactor.bindInteractors
+import com.softwaret.kpdf.response.bindResponses
 import com.softwaret.kpdf.routing.routes.login
 import com.softwaret.kpdf.util.instance
 import io.ktor.application.Application
 import io.ktor.application.install
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
+import io.ktor.features.PartialContent
+import io.ktor.jackson.jackson
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.Locations
 import io.ktor.routing.routing
@@ -17,9 +23,18 @@ fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun Application.main() {
 
     install(Locations)
+    install(ContentNegotiation) {
+        jackson {
+
+        }
+    }
+    install(DefaultHeaders)
+    install(PartialContent)
 
     di {
         bindControllers()
+        bindInteractors()
+        bindResponses()
     }
 
     routing {
