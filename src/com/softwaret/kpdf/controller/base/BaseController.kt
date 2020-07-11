52fb1d514d84cab6356bed920c54a1992d191746
Jan Starczewski <jan.a.starczewski@gmail.com>
2020-07-11 15:08:14 +0200

    Change imports in base controller

--- src/com/softwaret/kpdf/controller/base/BaseController.kt
+++ src/com/softwaret/kpdf/controller/base/BaseController.kt
@@ -1,8 +1,8 @@
 package com.softwaret.kpdf.controller.base
 
-import ErrorFactory
 import com.softwaret.kpdf.response.Response
 import com.softwaret.kpdf.response.error.ErrorType
+import com.softwaret.kpdf.response.error.factory.ErrorFactory
 import io.ktor.http.HttpStatusCode
 import java.io.Serializable
 
