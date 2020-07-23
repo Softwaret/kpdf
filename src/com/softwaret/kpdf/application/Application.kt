659569e38ba14acc6fa88d8e13c28d5179e124c7
Jan Starczewski <jan.a.starczewski@gmail.com>
2020-07-23 09:07:44 +0200

    Cleanup logic add way to inject parameters from Application.conf and extract Db setting to separate file

--- src/com/softwaret/kpdf/application/Application.kt
+++ src/com/softwaret/kpdf/application/Application.kt
@@ -1,5 +1,6 @@
 package com.softwaret.kpdf.application
 
+import com.auth0.jwt.algorithms.Algorithm
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.softwaret.kpdf.controller.bindControllers
 import com.softwaret.kpdf.db.Db
@@ -8,8 +9,12 @@ import com.softwaret.kpdf.routing.routes.login
 import com.softwaret.kpdf.routing.routes.register
 import com.softwaret.kpdf.service.bindServices
 import com.softwaret.kpdf.util.extension.instance
+import com.softwaret.kpdf.util.parameters.ServiceParameters
+import com.softwaret.kpdf.util.parameters.TokenServiceParameters
 import io.ktor.application.Application
 import io.ktor.application.install
+import io.ktor.auth.Authentication
+import io.ktor.auth.jwt.jwt
 import io.ktor.features.ContentNegotiation
 import io.ktor.features.DefaultHeaders
 import io.ktor.features.PartialContent
@@ -17,14 +22,17 @@ import io.ktor.jackson.jackson
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.Locations
 import io.ktor.routing.routing
+import io.ktor.util.KtorExperimentalAPI
 import org.kodein.di.ktor.di
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
+@KtorExperimentalAPI
 @KtorExperimentalLocationsAPI
 @Suppress("unused")
 fun Application.main() {
     installFeatures()
+    setupDb()
     bindDI()
     bindRouting()
 }
@@ -36,24 +44,48 @@ private fun Application.installFeatures() {
             configure(SerializationFeature.INDENT_OUTPUT, true)
         }
     }
+    install(Authentication) {
+        jwt {
+
+        }
+    }
     install(DefaultHeaders)
     install(PartialContent)
 }
 
-private fun Application.bindDI() {
+private fun setupDb() {
     Db.init()
+}
 
+@KtorExperimentalAPI
+private fun Application.bindDI() {
     di {
         bindControllers()
         bindInteractors()
-        bindServices()
+        bindServices(createServiceParameters())
     }
 }
 
 @KtorExperimentalLocationsAPI
 private fun Application.bindRouting() {
+
     routing {
         login(instance())
         register(instance())
     }
+}
+
+private const val EXPIRATION_TIME: Long = 300_00
+
+@KtorExperimentalAPI
+private fun Application.createServiceParameters(): ServiceParameters {
+
+    val secret = environment.config.property("jwt.COS_CO_BEDZIE_POTRZEBNE").getString()
+
+    return ServiceParameters(
+        TokenServiceParameters(
+            Algorithm.HMAC512(secret),
+            EXPIRATION_TIME
+        )
+    )
 }
