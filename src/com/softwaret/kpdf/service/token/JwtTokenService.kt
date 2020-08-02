31704e318781319da3a48aa8d433c7705fe7e4a8
Jan Starczewski <jan.a.starczewski@gmail.com>
2020-08-02 21:12:33 +0200

    Create JwtTokenService that is injected behind TokenService impl

--- src/com/softwaret/kpdf/service/token/JwtTokenService.kt
+++ src/com/softwaret/kpdf/service/token/JwtTokenService.kt
@@ -1,14 +1,37 @@
 package com.softwaret.kpdf.service.token
 
+import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.interfaces.JWTVerifier
+import com.softwaret.kpdf.service.user.UserService
 import java.util.*
 
 class JwtTokenService(
     private val algorithm: Algorithm,
-    private val expirationTimeMillis: Long
+    private val expirationTimeMillis: Long,
+    override val realm: String,
+    override val claimName: String,
+    private val userService: UserService,
+    private val issuer: String
 ) : TokenService {
 
-    override fun generateToken(claim: String): String = "TOKEN"
+    override val verifier: JWTVerifier by lazy {
+        JWT.require(algorithm)
+            .withIssuer(issuer)
+            .build()
+    }
+
+    override fun generateToken(claim: String): String =
+        JWT.create()
+            .withIssuer(issuer)
+            .withClaim(claimName, claim)
+            .withExpiresAt(getExpiration())
+            .sign(algorithm)
+
+    override fun validate(claimValue: String) =
+        userService.userByLogin(claimValue)?.let {
+            UserPrincipal(it.login)
+        }
 
     private fun getExpiration() = Date(System.currentTimeMillis() + expirationTimeMillis)
 }
--- src/com/softwaret/kpdf/service/token/TokenService.kt
+++ src/com/softwaret/kpdf/service/token/TokenService.kt
@@ -1,6 +1,17 @@
 package com.softwaret.kpdf.service.token
 
+import com.auth0.jwt.interfaces.JWTVerifier
+import io.ktor.auth.Principal
+
 interface TokenService {
 
+    val claimName: String
+
+    val verifier: JWTVerifier
+
+    val realm: String
+
     fun generateToken(claim: String): String
+
+    fun validate(claimValue: String): Principal?
 }
