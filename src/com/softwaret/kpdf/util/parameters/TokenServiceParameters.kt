aebea23f812e6ef88e6491198d0df54c3b95408a
Jan Starczewski <jan.a.starczewski@gmail.com>
2020-08-02 21:12:46 +0200

    Create TokenServiceParameters.kt

--- src/com/softwaret/kpdf/util/parameters/TokenServiceParameters.kt
+++ src/com/softwaret/kpdf/util/parameters/TokenServiceParameters.kt
@@ -1,8 +1,12 @@
 package com.softwaret.kpdf.util.parameters
 
 import com.auth0.jwt.algorithms.Algorithm
+import com.softwaret.kpdf.service.user.UserService
 
 data class TokenServiceParameters(
     val algorithm: Algorithm,
-    val expirationTime: Long
+    val expirationTime: Long,
+    val realm: String,
+    val claimName: String,
+    val issuer: String
 )
