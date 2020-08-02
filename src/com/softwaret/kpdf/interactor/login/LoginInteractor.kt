522739baffd58933c3a026859f6285d6cced786b
Jan Starczewski <jan.a.starczewski@gmail.com>
2020-08-02 21:11:32 +0200

    Inject tokenService and generate real token for user

--- src/com/softwaret/kpdf/interactor/login/LoginInteractor.kt
+++ src/com/softwaret/kpdf/interactor/login/LoginInteractor.kt
@@ -1,12 +1,10 @@
 package com.softwaret.kpdf.interactor.login
 
-import com.softwaret.kpdf.model.UserTile
-
 interface LoginInteractor {
 
     fun doesUserExists(login: String): Boolean
 
-    fun areCredentialsValid(login: String, password: String) : Boolean
+    fun areCredentialsValid(login: String, password: String): Boolean
 
-    fun generateToken(userTile: UserTile) : String
+    fun generateToken(login: String): String
 }
--- src/com/softwaret/kpdf/interactor/login/LoginInteractorImpl.kt
+++ src/com/softwaret/kpdf/interactor/login/LoginInteractorImpl.kt
@@ -1,10 +1,11 @@
 package com.softwaret.kpdf.interactor.login
 
-import com.softwaret.kpdf.model.UserTile
+import com.softwaret.kpdf.service.token.TokenService
 import com.softwaret.kpdf.service.user.UserService
 
 class LoginInteractorImpl(
-    private val userService: UserService
+    private val userService: UserService,
+    private val tokenService: TokenService
 ) : LoginInteractor {
 
     override fun doesUserExists(login: String) =
@@ -13,7 +14,6 @@ class LoginInteractorImpl(
     override fun areCredentialsValid(login: String, password: String) =
         userService.userByLogin(login)?.password == password
 
-    override fun generateToken(userTile: UserTile): String {
-        TODO("Not yet implemented")
-    }
+    override fun generateToken(login: String) =
+        tokenService.generateToken(login)
 }
