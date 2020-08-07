66b6500c386980a0b93b9fcbb4dc70847fbf997f
janstoltman <stoltmanjan@gmail.com>
2020-08-07 15:53:20 +0200

    Remove unused extensions

--- src/com/softwaret/kpdf/util/extension/ParametersExtension.kt
+++
@@ -1,8 +0,0 @@
-package com.softwaret.kpdf.util.extension
-
-import io.ktor.http.Parameters
-
-operator fun Parameters.get(bodyParameter: BodyParameter) = get(bodyParameter.parameterName)
-
-operator fun Parameters.get(vararg bodyParameters: BodyParameter) =
-    bodyParameters.map { param -> get(param) }
