a885e362a593833855acbca30d237ace45daf821
janstoltman <stoltmanjan@gmail.com>
2020-08-17 21:23:56 +0200

    Refactor HashRepository.kt

--- src/com/softwaret/kpdf/repository/hash/HashRepository.kt
+++ src/com/softwaret/kpdf/repository/hash/HashRepository.kt
@@ -2,5 +2,5 @@ package com.softwaret.kpdf.repository.hash
 
 interface HashRepository {
 
-    fun hashString(raw: String): String
+    fun hash(raw: String): String
 }
--- src/com/softwaret/kpdf/repository/hash/SaltingHashRepository.kt
+++ src/com/softwaret/kpdf/repository/hash/SaltingHashRepository.kt
@@ -7,10 +7,19 @@ class SaltingHashRepository(
     private val salt: String
 ) : HashRepository {
 
-    override fun hashString(raw: String): String {
-        requireNotNullAndNotBlank(raw) { "String to be hashed should not be blank" }
+    companion object {
+        private enum class HashType(val raw: String) {
+            SHA_512("SHA-512")
+        }
+    }
 
-        val digest = MessageDigest.getInstance("SHA-256").digest("$salt$raw".toByteArray())
-        return digest.fold("", { str, it -> str + "%02x".format(it) })
+    override fun hash(raw: String): String {
+        requireNotNullAndNotBlank(raw) { "String to be hashed should not be blank" }
+        return hash(raw, HashType.SHA_512)
+    }
+
+    private fun hash(raw: String, type: HashType): String {
+        val digest = MessageDigest.getInstance(type.raw).digest("$salt$raw".toByteArray())
+        return digest.fold("", { hash, chunk -> hash + "%02x".format(chunk) })
     }
 }
