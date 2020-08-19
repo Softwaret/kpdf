b84d8076800351fda3e870a1b2857b5948e51d82
Jan Starczewski <jan.a.starczewski@gmail.com>
2020-08-19 08:53:42 +0200

    Fix import

--- src/com/softwaret/kpdf/service/token/JWTTokenVeryfingServiceImpl.kt
+++ src/com/softwaret/kpdf/service/token/JWTTokenVeryfingServiceImpl.kt
@@ -2,10 +2,11 @@ package com.softwaret.kpdf.service.token
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import com.auth0.jwt.interfaces.JWTVerifier
 
 open class JWTTokenVeryfingServiceImpl : JWTTokenVeryfingService {
 
-    override fun buildVerifier(algorithm: Algorithm, issuer: String): com.auth0.jwt.JWTVerifier =
+    override fun buildVerifier(algorithm: Algorithm, issuer: String): JWTVerifier =
         JWT.require(algorithm)
             .withIssuer(issuer)
             .build()
