7d600fc01ae79bbb1b6e0d45f9d5c369d089b1fe
Jan Starczewski <jan.a.starczewski@gmail.com>
2020-09-04 20:07:16 +0200

    Creat InputValidator.kt

---
+++ src/com/softwaret/kpdf/validation/InputValidator.kt
@@ -0,0 +1,20 @@
+package com.softwaret.kpdf.validation
+
+import com.softwaret.kpdf.validation.result.ValidationResult
+import com.softwaret.kpdf.validation.validators.Validator
+import org.kodein.di.DI
+import org.kodein.di.DIAware
+import org.kodein.di.direct
+import org.kodein.di.instance
+
+class InputValidator(
+    override val di: DI
+) : DIAware {
+
+    inline fun <reified T> validate(param: T) =
+        (di.direct.instance<Validator<*>>(tag = T::class.java) as? Validator<T>)?.validate(param)
+            ?: ValidationResult.GenericError
+
+    inline fun <reified T> T.isValid() =
+        validate(this) == ValidationResult.Valid
+}
