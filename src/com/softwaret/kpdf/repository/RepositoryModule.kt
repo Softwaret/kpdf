9fb050af28da2b79fdce11f5b75efb3f89421e4c
Jan Starczewski <jan.a.starczewski@gmail.com>
2020-09-04 20:07:52 +0200

    Delete unused bindings

--- src/com/softwaret/kpdf/repository/RepositoryModule.kt
+++ src/com/softwaret/kpdf/repository/RepositoryModule.kt
@@ -2,12 +2,6 @@ package com.softwaret.kpdf.repository
 
 import com.softwaret.kpdf.repository.hash.HashRepository
 import com.softwaret.kpdf.repository.hash.SaltingHashRepository
-import com.softwaret.kpdf.repository.validation.input.login.LoginValidationRepository
-import com.softwaret.kpdf.repository.validation.input.login.LoginValidationRepositoryImpl
-import com.softwaret.kpdf.repository.validation.input.name.NameValidationRepository
-import com.softwaret.kpdf.repository.validation.input.name.NameValidationRepositoryImpl
-import com.softwaret.kpdf.repository.validation.input.password.PasswordValidationRepository
-import com.softwaret.kpdf.repository.validation.input.password.PasswordValidationRepositoryImpl
 import com.softwaret.kpdf.util.parameters.SALT
 import org.kodein.di.DI
 import org.kodein.di.bind
@@ -15,12 +9,6 @@ import org.kodein.di.instance
 import org.kodein.di.singleton
 
 fun DI.MainBuilder.bindPreferences() {
-
-    bind<LoginValidationRepository>() with singleton { LoginValidationRepositoryImpl() }
-
-    bind<NameValidationRepository>() with singleton { NameValidationRepositoryImpl() }
-
-    bind<PasswordValidationRepository>() with singleton { PasswordValidationRepositoryImpl() }
 
     bind<HashRepository>() with singleton { SaltingHashRepository(instance(tag = SALT)) }
 }
