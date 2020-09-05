90c6c519aac5d24f2eed9c6c624b23d4f237c01f
janstoltman <stoltmanjan@gmail.com>
2020-09-05 21:52:16 +0200

    Expand UserTest

--- test/com/softwaret/kpdf/db/tables/user/UserTest.kt
+++ test/com/softwaret/kpdf/db/tables/user/UserTest.kt
@@ -1,9 +1,11 @@
 package com.softwaret.kpdf.db.tables.user
 
 import com.softwaret.kpdf.db.tables.base.test.BaseTableTest
+import org.jetbrains.exposed.exceptions.ExposedSQLException
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import kotlin.test.assertNotNull
 
 class UserTest : BaseTableTest() {
@@ -25,4 +27,41 @@ class UserTest : BaseTableTest() {
         }
     }
 
+    @Test
+    fun `create new user should fail when missing arguments`() {
+        assertThrows<ExposedSQLException> {
+            transaction {
+                User.new {
+                    login = "login"
+                    name = "name"
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `create new user should fail with login over 50 chars`() {
+        assertThrows<IllegalStateException> {
+            transaction {
+                User.new {
+                    login = "a".repeat(51)
+                    password = "password"
+                    name = "name"
+                }
+            }
+        }
+    }
+
+    @Test
+    fun `create new user should pass with password length equal 200 chars`() {
+        transaction {
+            val createdUser = User.new {
+                login = "login"
+                password = "p".repeat(200)
+                name = "name"
+            }
+
+            assertNotNull(User.findById(createdUser.id))
+        }
+    }
 }
