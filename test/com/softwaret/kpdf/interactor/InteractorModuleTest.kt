350f402a5f25be514e58f99f577f30dd4ea9f7d2
janstoltman <stoltmanjan@gmail.com>
2020-09-08 12:14:50 +0200

    Fix tests

--- test/com/softwaret/kpdf/interactor/InteractorModuleTest.kt
+++ test/com/softwaret/kpdf/interactor/InteractorModuleTest.kt
@@ -3,7 +3,7 @@ package com.softwaret.kpdf.interactor
 import com.softwaret.kpdf.base.test.BaseModuleTest
 import org.junit.jupiter.api.Test
 
-internal class InteractorModuleKtTest : BaseModuleTest() {
+internal class InteractorModuleTest : BaseModuleTest() {
 
     @Test
     override fun testBind() {
--- test/com/softwaret/kpdf/util/parameters/ParameterModuleTest.kt
+++ test/com/softwaret/kpdf/util/parameters/ParameterModuleTest.kt
@@ -1,13 +1,12 @@
 package com.softwaret.kpdf.util.parameters
 
 import com.softwaret.kpdf.base.test.BaseModuleTest
-import io.mockk.mockk
 import org.junit.jupiter.api.Test
 
 class ParameterModuleTest : BaseModuleTest() {
 
     @Test
     override fun testBind() {
-        builder.bindParameters(mockk())
+        builder.bindParameters("salt")
     }
 }
