f68c721c66fe16c84eb782d454f2b4024281e33b
Jan Starczewski <jan.a.starczewski@gmail.com>
2020-09-15 21:47:16 +0200

    Add field to agregate errors

--- src/com/softwaret/kpdf/validation/result/ValidationResult.kt
+++ src/com/softwaret/kpdf/validation/result/ValidationResult.kt
@@ -2,11 +2,19 @@ package com.softwaret.kpdf.validation.result
 
 sealed class ValidationResult {
 
-    object GenericError: ValidationResult()
+    val results: List<ValidationResult> = mutableListOf()
+
+    fun addResults(validationResult: List<ValidationResult>) {
+        (results as MutableList<ValidationResult>).addAll(validationResult)
+    }
+
+    object GenericError : ValidationResult()
 
     object Empty : ValidationResult()
 
     object Blank : ValidationResult()
 
     object Valid : ValidationResult()
+
+    object Result : ValidationResult()
 }
