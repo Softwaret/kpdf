5df993bf5e516cff73e11ad7565a5820677896b9
Jan Starczewski <jan.a.starczewski@gmail.com>
2021-02-28 12:38:50 +0100

    Create log object to abstract loggin in non application classes

---
+++ src/com/softwaret/kpdf/util/log/Log.kt
@@ -0,0 +1,14 @@
+package com.softwaret.kpdf.util.log
+
+import mu.KLogger
+import mu.KotlinLogging
+
+object Log {
+
+    private const val loggerName = "KPDF-DEBUG: "
+
+    private val logger: KLogger by lazy { KotlinLogging.logger(loggerName) }
+
+    fun d(message: () -> String) =
+        logger.takeIf { it.isDebugEnabled }?.debug(message)
+}
