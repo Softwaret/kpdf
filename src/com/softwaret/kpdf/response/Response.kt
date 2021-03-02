8b572a7b2a490470cc48283ac2d5815963622fd0
Jan Starczewski <jan.a.starczewski@gmail.com>
2021-03-02 23:18:57 +0100

    Add responses codes

--- src/com/softwaret/kpdf/response/Response.kt
+++ src/com/softwaret/kpdf/response/Response.kt
@@ -20,8 +20,17 @@ fun Response.Companion.Unauthorized(body: ResponseBody) =
 fun Response.Companion.OK(body: ResponseBody) =
     Response(HttpStatusCode.OK, body)
 
+fun Response.Companion.Created(body: ResponseBody) =
+    Response(HttpStatusCode.Created, body)
+
 fun Response.Companion.BadRequest(body: ResponseBody) =
     Response(HttpStatusCode.BadRequest, body)
 
 fun Response.Companion.NotFound(body: ResponseBody) =
     Response(HttpStatusCode.NotFound, body)
+
+fun Response.Companion.InternalServerError(body: ResponseBody) =
+    Response(HttpStatusCode.InternalServerError, body)
+
+fun Response.Companion.Conflict(body: ResponseBody) =
+    Response(HttpStatusCode.Conflict, body)
