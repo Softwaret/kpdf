e92804f489daa2a126eb1be49369b0d556318ec0
Jan Starczewski <jan.a.starczewski@gmail.com>
2021-03-12 10:24:46 +0100

    Add input stream extension to read all bytes

--- src/com/softwaret/kpdf/util/extension/InputStream.kt
+++ src/com/softwaret/kpdf/util/extension/InputStream.kt
@@ -1,5 +1,20 @@
 package com.softwaret.kpdf.util.extension
 
+import com.softwaret.kpdf.util.log.Log
+import java.io.IOException
 import java.io.InputStream
 
 fun InputStream.readString() = bufferedReader().use { it.readText() }
+
+fun InputStream.trySafeReadingOrEmpty(): ByteArray =
+    try {
+        val allBytes = readAllBytes()
+        close()
+        allBytes
+    } catch (exception: IOException) {
+        Log.d {
+            "Reading stream error"
+        }
+        Log.e(exception)
+        ByteArray(0)
+    }
