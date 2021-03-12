33d4b6a0f51ac0f6a70cbc12db4c979d5d01d154
Jan Starczewski <jan.a.starczewski@gmail.com>
2021-03-12 10:22:20 +0100

    Create file response to respond with files

---
+++ src/com/softwaret/kpdf/response/file/FileResponse.kt
@@ -0,0 +1,38 @@
+package com.softwaret.kpdf.response.file
+
+import io.ktor.http.*
+
+data class FileResponse(
+    val code: HttpStatusCode,
+    val fileName: String,
+    val file: ByteArray
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as FileResponse
+
+        if (code != other.code) return false
+        if (fileName != other.fileName) return false
+        if (!file.contentEquals(other.file)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = code.hashCode()
+        result = 31 * result + fileName.hashCode()
+        result = 31 * result + file.contentHashCode()
+        return result
+    }
+
+    companion object
+}
+
+fun FileResponse.Companion.ok(fileName: String, file: ByteArray) =
+    FileResponse(HttpStatusCode.OK, fileName, file)
+
+fun FileResponse.Companion.emptyNotFound() =
+    FileResponse(HttpStatusCode.NotFound, "", ByteArray(0))
