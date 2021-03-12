51b825c8332f95b7f99263fd1a1f925eb1dfc2fc
Jan Starczewski <jan.a.starczewski@gmail.com>
2021-03-12 10:03:27 +0100

    Change impl to publication service

--- src/com/softwaret/kpdf/service/publication/PublicationsServiceImpl.kt
+++ src/com/softwaret/kpdf/service/publication/PublicationsServiceImpl.kt
@@ -10,8 +10,8 @@ import com.softwaret.kpdf.db.tables.user.Users
 import com.softwaret.kpdf.model.inline.*
 import com.softwaret.kpdf.util.exception.PublicationException
 import com.softwaret.kpdf.util.extension.asId
+import org.jetbrains.exposed.exceptions.ExposedSQLException
 import org.jetbrains.exposed.sql.transactions.transaction
-import java.util.*
 import javax.sql.rowset.serial.SerialBlob
 
 class PublicationsServiceImpl : PublicationsService {
@@ -29,18 +29,21 @@ class PublicationsServiceImpl : PublicationsService {
 
     override fun insertPublication(
         publicationName: PublicationName,
-        pdfBase64: PdfBase64,
+        pdfFile: PdfFile,
         login: Login,
         description: Description
-    ) =
+    ) = try {
         transaction {
             Publication.new {
                 name = publicationName.value
                 author = User.find { Users.login eq login.value }.first()
-                pdf = Pdf.new { blobPdf = SerialBlob(Base64.getDecoder().decode(pdfBase64.value)) }
+                pdf = Pdf.new { blobPdf = SerialBlob(pdfFile.bytes) }
                 metadata = Metadata.new { this.description = description.value }
             }
         }.id.value.asId()
+    } catch (e: ExposedSQLException) {
+        throw PublicationException.PublicationNotCreated
+    }
 
     override fun deletePublication(id: Id) {
         transaction {
@@ -48,10 +51,10 @@ class PublicationsServiceImpl : PublicationsService {
         }
     }
 
-    override fun updatePublication(id: Id, pdfBase64: PdfBase64) {
+    override fun updatePublication(id: Id, pdfFile: PdfFile) {
         transaction {
             Publication.findById(id.value)?.let {
-                it.pdf.blobPdf = SerialBlob(pdfBase64.value.toByteArray())
+                it.pdf.blobPdf = SerialBlob(pdfFile.bytes)
             } ?: throw PublicationException.PublicationNotFound
         }
     }
