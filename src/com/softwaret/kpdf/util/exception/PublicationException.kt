1512f1bfee91410ab3d3e9f5dcf183eab3382a55
Jan Starczewski <jan.a.starczewski@gmail.com>
2021-03-12 10:25:12 +0100

    Create publication exception about not found publication

--- src/com/softwaret/kpdf/util/exception/PublicationException.kt
+++ src/com/softwaret/kpdf/util/exception/PublicationException.kt
@@ -3,4 +3,6 @@ package com.softwaret.kpdf.util.exception
 sealed class PublicationException(message: String?) : Throwable(message) {
 
     object PublicationNotFound : PublicationException("Publication has not been found")
+
+    object PublicationNotCreated : PublicationException("Publication has not been created")
 }
